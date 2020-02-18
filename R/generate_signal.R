931b9ad633b1cb85caf4adbd5d997b33cb45e024
David Parr <davidrgparr@gmail.com>
2020-02-18 14:45:25 +0000

    state package purpose

--- R/generate_signal.R
+++ R/generate_signal.R
@@ -15,13 +15,26 @@
 #' @export
 #' @importFrom tibble tibble
 
-pure_signal <- function(x) {
-  intervals <- x * 48
+pure_signal <- function(days = 7 * 4, start = "2020-01-01") {
+  intervals <- days * 48
 
-  df <- tibble::tibble("generated_intervals" = seq(from = 0,
-                                               to = (x * 2) * pi,
-                                               length.out = intervals),
-                       "cos_signal" =-cos(generated_intervals)
+  start_date <- as.Date(start)
+
+  end_date <- start_date + days
+
+  date_time <- seq(as.POSIXct(start_date),
+                   as.POSIXct(end_date),
+                   by = "30 min")[1:intervals]
+
+  df <- tibble::tibble(
+    date_time = date_time,
+    pintervals = seq(
+      from = 0,
+      to = (days * 2) * pi,
+      length.out = intervals
+    ),
+    pure = -cos(pintervals),
+    signal = pure
   )
   return(df)
 }
@@ -46,7 +59,9 @@ pure_signal <- function(x) {
 
 shift_signal <- function(x, centre_point = 10) {
 
-  df <- dplyr::mutate(x, shifted_signal = cos_signal + 10)
+  df <- dplyr::mutate(x,
+                      shift = 10,
+                      signal = signal + shift)
 
   return(df)
 }
@@ -72,7 +87,51 @@ shift_signal <- function(x, centre_point = 10) {
 
 add_noise <- function(x, low=0, high = 1) {
 
-  df <- dplyr::mutate(x, noise = runif(nrow(x), low, high))
+  df <- dplyr::mutate(x,
+                      noise = runif(nrow(x), low, high),
+                      signal = signal + noise)
 
+  return(df)
+}
+
+#' @title add seasonality to a signal
+#' @description FUNCTION_DESCRIPTION
+#' @param x PARAM_DESCRIPTION
+#' @param seasonality_strength PARAM_DESCRIPTION, Default: 5
+#' @return OUTPUT_DESCRIPTION
+#' @details DETAILS
+#' @examples
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @rdname add_seasonality
+#' @export
+#' @importFrom dplyr mutate
+
+add_seasonality <- function(x, seasonality_strength = 5) {
+  seasonality_seq <- cos(seq(
+    from = 0,
+    to = pi * 2,
+    length.out = 365 * 48
+  )) * seasonality_strength
+
+  intervals <- nrow(x)
+
+  annual_length <- floor(intervals / (365 * 48))
+
+  if (annual_length > 0) {
+    remainder_length <- intervals - length(seasonality_seq)
+
+    df <- dplyr::mutate(x,
+                        seasonality = append(rep(seasonality_seq, annual_length),
+                                             seasonality_seq[1:remainder_length]),
+                        signal = signal + seasonality)
+  } else {
+    df <- dplyr::mutate(x,
+                        seasonality = seasonality_seq[1:intervals],
+                        signal = signal + seasonality)
+  }
   return(df)
 }
